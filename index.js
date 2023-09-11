10c95e7f2d5c31b8aaeef613d26789b7464e7bc8
Ikquilibrium-SG <ikquisamtance10@gmail.com>
2023-09-11 16:03:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ const app = express()
 
 const moment = require('moment')
 
-app.get('/', (req, res) => {
-    const slackName = 'Sammyboi'
+app.get('/api', (req, res) => {
+    const slackName = req.query.slack_name
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const currentDay = daysOfWeek[moment().day()]
     const utcTime = moment().utc().format()
-    const track = 'backend'
-    const githubFileUrl = "";
-    const githubRepoUrl = "";
+    const track = req.query.track
+    const githubFileUrl = "https://github.com/Ikquilibrium-SG/project-1/blob/main/index.js";
+    const githubRepoUrl = "https://github.com/Ikquilibrium-SG/project-1";
 
     const responseJson = {
-        slack_name: slackName,
+        slack_name: "Sammyboi",
         current_day: currentDay,
         utc_time: utcTime,
-        track: track,
+        track: "backend",
         github_file_url: githubFileUrl,
         github_repo_url: githubRepoUrl,
         status_code: 200
     };
 
     res.json(responseJson)
+
+    const apiEndpoint = 'https://ikquilibrium-sg.github.io/project-1/api'
+
+    const fullUrl = `${apiEndpoint}?slack_name=${responseJson.slack_name}&track=${responseJson.track}`
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000')
+
+app.listen(80, () => {
+    console.log('Server is running on port 80')
 })
